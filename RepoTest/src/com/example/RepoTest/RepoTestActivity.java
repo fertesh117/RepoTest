8df570435fa719b341cf65127d2620a06cf3796e
fertesh117 <fertesh117@gmail.com>
2011-10-24 14:25:06 -0500

    First Commit

---
+++ RepoTest/src/com/example/RepoTest/RepoTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.RepoTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RepoTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
